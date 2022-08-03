aab32787346d64d54da04112ef4df28edfc4a85c
Trend <Trend778@gmail.com>
2022-08-03 20:50:35 +0400

    Master Commit

---
+++ src/main/java/pro/sky/course2/collectionslistssets/CollectionsListsSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.collectionslistssets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsListsSetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsListsSetsApplication.class, args);
+    }
+
+}
